60e10ac3acd56b6fb90edfad2d87fd47c8bc6c6b
shankee1506 <pshankeerthan@gmail.com>
2022-09-03 12:44:17 +0530

    Initialize project

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import Navbar from './components/Navbar';
+
+
+function App() {
+  return (
+<div >
+      <Navbar />
+      </div>
+
+  );
+}
+
+export default App;
